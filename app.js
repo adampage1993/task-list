008ca33ebf9d17400360defe4abb663c886dacdb
adampage1993 <63195949+adampage1993@users.noreply.github.com>
2020-07-25 09:05:22 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,41 @@ const clearBtn = document.querySelector(".clear-tasks");
 const filter = document.querySelector("#filter");
 const taskInput = document.querySelector("#task");
 
+// Load All Event Listeners
+loadEventListeners();
+
+// Load All Event Listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add Class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon HTML
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append link to the li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li)
+
+  // Clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
